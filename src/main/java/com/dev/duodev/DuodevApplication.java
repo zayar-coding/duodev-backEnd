b2154beb15ecdbec99d6066215f36911e76325a5
zayarmyooo19 <hanzohasashi880@gmail.com>
2023-09-23 13:07:43 +0630

    project setup

---
+++ src/main/java/com/dev/duodev/DuodevApplication.java
@@ -0,0 +1,13 @@
+package com.dev.duodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuodevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuodevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/duodev/DuodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.duodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuodevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
